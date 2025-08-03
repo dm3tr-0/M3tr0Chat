0c93351e35c146305034f2a8d034d1959c0ada46
mitya <me@dm3tr0.ru>
2025-08-03 16:48:47 +0500

    update fix

--- m3tr0chat.py
+++ m3tr0chat.py
@@ -24,12 +24,16 @@ onion_address = None
 app = Flask(__name__)
 received_messages = []
 
+bridge_bin = ''
+
 def get_tor_path():
     """Возвращает путь к tor.exe/tor в зависимости от ОС"""
     if sys.platform == "win32":
+        bridge_bin = "lyrebird.exe"
         return os.path.join(TOR_DIR, "tor.exe")
     else:
-        return os.path.join(TOR_DIR, "tor")
+        bridge_bin = "lyrebird"
+        return os.path.join(TOR_DIR, "libTor.so")
 
 def get_tor_config():
     """Возвращает конфиг с мостами"""
@@ -39,7 +43,7 @@ def get_tor_config():
         "DataDirectory": TOR_DATA_DIR,
         "UseBridges": "1",
         #"ClientTransportPlugin": "obfs4 exec ./tor/obfs4proxy.exe",
-        "ClientTransportPlugin": "snowflake,obfs4,meek_lite,obfs2,obfs3,scramblesuit,webtunnel exec ./tor/pluggable_transports/lyrebird.exe",
+        "ClientTransportPlugin": "snowflake,obfs4,meek_lite,obfs2,obfs3,scramblesuit,webtunnel exec ./tor/pluggable_transports/{bridge_bin}}",
         "Bridge":[
             "snowflake 192.0.2.3:80 2B280B23E1107BB62ABFC40DDCC8824814F80A72 fingerprint=2B280B23E1107BB62ABFC40DDCC8824814F80A72 url=https://1098762253.rsc.cdn77.org/ fronts=www.cdn77.com,www.phpmyadmin.net ice=stun:stun.antisip.com:3478,stun:stun.epygi.com:3478,stun:stun.uls.co.za:3478,stun:stun.voipgate.com:3478,stun:stun.mixvoip.com:3478,stun:stun.nextcloud.com:3478,stun:stun.bethesda.net:3478,stun:stun.nextcloud.com:443 utls-imitate=hellorandomizedalpn",
             "snowflake 192.0.2.4:80 8838024498816A039FCBBAB14E6F40A0843051FA fingerprint=8838024498816A039FCBBAB14E6F40A0843051FA url=https://1098762253.rsc.cdn77.org/ fronts=www.cdn77.com,www.phpmyadmin.net ice=stun:stun.antisip.com:3478,stun:stun.epygi.com:3478,stun:stun.uls.co.za:3478,stun:stun.voipgate.com:3478,stun:stun.mixvoip.com:3478,stun:stun.nextcloud.com:3478,stun:stun.bethesda.net:3478,stun:stun.nextcloud.com:443 utls-imitate=hellorandomizedalpn",
@@ -112,7 +116,7 @@ def receive_message():
     
     if message:
         temp = message.split('\n', 1)
-        sender = 'http://' + str(temp[0])
+        sender = str(temp[0])
         try:
             message = str(temp[1])
         except Exception as ex:
@@ -150,7 +154,7 @@ def send_message(target_onion, message):
         
         response = session.post(
             url,
-            data={'message': str('http' + str(onion_address))+ '\n' + str(message)},
+            data={'message': str('http://' + str(onion_address))+ '\n' + str(message)},
             timeout=50,
             headers={'Connection': 'close'}
         )
